5949364bb0befb662c95c7f436dcc27444d1181f
RafaelDJ789 <ragonzalez.sando2@gmail.com>
2019-05-31 10:54:18 -0600

    commit1

---
+++ Repaso.py
@@ -0,0 +1,13 @@
+from tkinter import ttk
+from tkinter import *
+
+window=Tk()
+window.title("k honda")
+a= str (100)
+b= str (200)
+
+
+window.geometry(a+"x"+b)
+
+frame=contenedor(self.window,text="Cuadro Ddonde va a ir la suma",background "pink")
+window.mainloop()
